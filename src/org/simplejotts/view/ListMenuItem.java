9284479b040e97d4aaa4efa808c77922619c65d5
Richard Varela <richardvare13@gmail.com>
2025-05-30 10:43:37 -0700

    Changed how the list look in View. Now there's a date on the left and the preview on the right. It looks good though. Changed the ListMenuItem to handle the case of the note being greater than 70 to have elipses.

--- src/org/simplejotts/view/ListMenuItem.java
+++ src/org/simplejotts/view/ListMenuItem.java
@@ -8,12 +8,17 @@ public class ListMenuItem {
 	private LocalDateTime dateCreated;
 
 	public ListMenuItem(final String noteContent, final LocalDateTime dateCreated) {
-		this.preview = noteContent.substring(0, 10);
+		if (noteContent.length() > 70) {
+			this.preview = noteContent.substring(0, 70).concat("...");
+		}
+		else {
+			this.preview = noteContent;
+		}
 		this.dateCreated = dateCreated;
 	}
 
 	@Override
 	public String toString() {
-		return String.format("%s...", this.preview);
+		return String.format("%10s    %-80s", this.dateCreated.toLocalDate(), this.preview);
 	}
 }
